e4e0acf655e6d9b5562545d7bb265d56aa05801e
Lazzari <baptiste.lazzari@outlook.fr>
2023-01-07 15:21:26 +0100

    exo2_4

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :movie
-  resources :list, only: %i[new create index show] do
-    resources :bookmark, only: %i[new create]
+  resources :movies
+  resources :lists, only: %i[new create index show] do
+    resources :bookmarks, only: %i[new create]
   end
 end
+
+# rails g controller movies index show new create edit update destroy
+# rails g controller bookmarks new create destroy show
+# rails g controller lists index show new create
