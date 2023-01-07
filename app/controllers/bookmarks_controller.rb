49e2e1c7013e347eae1afdfada54bb277ee943bf
Lazzari <baptiste.lazzari@outlook.fr>
2023-01-07 17:00:16 +0100

    exo2 stop

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,16 +1,29 @@
 class BookmarksController < ApplicationController
   def new
     @bookmark = Bookmark.new
-    @list = List.find([params[:list_id]])
+    @list = List.find(params[:list_id])
     @movies = Movie.all
   end
 
   def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(review_params)
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
   end
 
   def show
+  end
+
+  private
+
+  def review_params
+    params.require(:bookmark).permit(:comment, :list_id, :moovie_id)
   end
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,5 @@
 class List < ApplicationRecord
-  validates :name, presence: true
-  validates_uniqueness_of :name
+  validates :name, presence: true, uniqueness: true
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
 end
