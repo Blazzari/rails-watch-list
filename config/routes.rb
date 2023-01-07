4dbb9b8cb87d9bb5e86b1489c406838d8b4376f3
Lazzari <baptiste.lazzari@outlook.fr>
2023-01-07 14:23:40 +0100

    exo2_3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :movie
+  resources :list, only: %i[new create index show] do
+    resources :bookmark, only: %i[new create]
+  end
 end
