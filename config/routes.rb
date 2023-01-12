9924bccec4a7b4266fc8374bc3afa20b1808c1b6
Lazzari <baptiste.lazzari@outlook.fr>
2023-01-12 20:33:14 +0100

    ajout root lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'lists#index'
   resources :movies
   resources :lists, only: %i[new create index show] do
     resources :bookmarks, only: %i[new create]
